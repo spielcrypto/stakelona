9a626307d75747a35473a20f49e5f946a39c9c35
Spielcrypto <spieljs@gmail.com>
2025-10-05 18:38:34 +0500

    fix next config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/stakelona' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/stakelona/' : '',
 }
 
 module.exports = nextConfig
